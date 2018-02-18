0771644c3566544c822c8632367cfb3dac7f557b
Andi <a.hein@andreas-hein.net>
2018-02-18 17:24:40 +0100

    add an internal (after login) menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,8 @@
                             <li><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                             <li><a class="nav-link" href="{{ route('register') }}">Register</a></li>
                         @else
+							<li><a class="nav-link" href="{{ url('/home') }}">Home</a></li>
+							<li><a class="nav-link" href="{{ url('/tasks') }}">Task List</a></li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
